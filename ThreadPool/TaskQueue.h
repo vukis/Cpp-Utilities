632eb40607dc153d975b88aaf49a14d68f5d4ec4
vukis <vukis@bk.ru>
2017-07-16 15:30:24 +0300

    Formatting changes

--- ThreadPool/TaskQueue.h
+++ ThreadPool/TaskQueue.h
@@ -10,7 +10,7 @@ class TaskQueue
 {
 public:
 
-	using TaskType = std::function<void()>;
+    using TaskType = std::function<void()>;
 
     TaskQueue() = default;
     ~TaskQueue() = default;
@@ -18,64 +18,64 @@ public:
     TaskQueue(TaskQueue&&) = default;
     TaskQueue& operator=(TaskQueue&&) = default;
 
-	void SetDone(bool done)
-	{
-		{
-			LockType lock{ m_mutex };
-			m_done = done;
-		}
+    void SetDone(bool done)
+    {
+        {
+            LockType lock{ m_mutex };
+            m_done = done;
+        }
 
-		if (done)
-			m_ready.notify_all();
-	}
+        if (done)
+            m_ready.notify_all();
+    }
 
-	auto IsDone() const
-	{
-		LockType lock{ m_mutex };
-		return m_done;
-	}
+    auto IsDone() const
+    {
+        LockType lock{ m_mutex };
+        return m_done;
+    }
 
-	auto WaitAndPop(TaskType& task)
-	{
-		LockType lock{ m_mutex };
+    auto WaitAndPop(TaskType& task)
+    {
+        LockType lock{ m_mutex };
 
-		while (m_queue.empty() && !m_done) 
-			m_ready.wait(lock);
-		
-		if (!m_queue.empty() && !m_done)
-		{
-			task = std::move(m_queue.front());
-			m_queue.pop_front();
-			return true;
-		}
+        while (m_queue.empty() && !m_done)
+            m_ready.wait(lock);
 
-		return false;
-	}
+        if (!m_queue.empty() && !m_done)
+        {
+            task = std::move(m_queue.front());
+            m_queue.pop_front();
+            return true;
+        }
 
-	template<typename TTask>
-	auto Push(TTask&& task) // -> std::future<decltype(task())>
-	{
-		using TaskReturnType = decltype(task());
+        return false;
+    }
+
+    template<typename TTask>
+    auto Push(TTask&& task) // -> std::future<decltype(task())>
+    {
+        using TaskReturnType = decltype(task());
 
         // std::packaged_task<> is move only type.
         // We need to wrap it in a shared_ptr:
-		auto packagedTask = std::make_shared<std::packaged_task<TaskReturnType()>>(std::forward<TTask>(task));
-		auto future = packagedTask->get_future();
+        auto packagedTask = std::make_shared<std::packaged_task<TaskReturnType()>>(std::forward<TTask>(task));
+        auto future = packagedTask->get_future();
 
-		{
-			LockType lock{ m_mutex };
-			m_queue.emplace_back([packagedTask] { (*packagedTask)(); });
-		}
+        {
+            LockType lock{ m_mutex };
+            m_queue.emplace_back([packagedTask] { (*packagedTask)(); });
+        }
 
-		m_ready.notify_one();
-		return future;
-	}
+        m_ready.notify_one();
+        return future;
+    }
 
-    auto TryPop(TaskType& task) 
+    auto TryPop(TaskType& task)
     {
         LockType lock{ m_mutex, std::try_to_lock };
 
-        if (!lock || m_queue.empty()) 
+        if (!lock || m_queue.empty())
             return false;
 
         task = move(m_queue.front());
@@ -108,12 +108,12 @@ public:
 
 private:
 
-	using LockType = std::unique_lock<std::mutex>;
+    using LockType = std::unique_lock<std::mutex>;
 
-	std::deque<TaskType> m_queue;
-	bool                 m_done{ false };
-	mutable std::mutex      m_mutex;
-	std::condition_variable m_ready;
+    std::deque<TaskType> m_queue;
+    bool                 m_done{ false };
+    mutable std::mutex      m_mutex;
+    std::condition_variable m_ready;
 
     TaskQueue(const TaskQueue&) = delete;
     TaskQueue& operator=(const TaskQueue&) = delete;
