a898a374064efa1d1ef94e30fb78db1506121705
vukis <vukis@bk.ru>
2017-07-25 01:50:39 +0300

    add set enviroment script

---
+++ CI/AppVeyor/SetEnviroment.bat
@@ -0,0 +1,48 @@
+:loop
+
+if "%1" == "" goto :finalize
+if /i "%1" == "x86" goto :x86
+if /i "%1" == "x64" goto :x64
+if /i "%1" == "msvc15" goto :msvc15
+
+echo Invalid argument: '%1'
+exit -1
+
+:: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
+:: Platform
+
+:x86
+set TARGET_CPU=x86
+set CMAKE_GENERATOR_SUFFIX=
+shift
+goto :loop
+
+:x64
+set TARGET_CPU=x64
+set CMAKE_GENERATOR_SUFFIX= Win64
+shift
+goto :loop
+
+:: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
+:: Toolchain
+
+:msvc15
+set TOOLCHAIN=msvc15
+set CMAKE_GENERATOR=Visual Studio 15 2017
+shift
+goto :loop
+
+:: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
+:finalize
+
+echo Config: %CONFIGURATION%
+
+if "%TOOLCHAIN%" == "" goto :msvc15
+if "%TARGET_CPU%" == "" goto :x86
+if "%CONFIGURATION%" == "" (set CONFIGURATION=Release)
+
+set CMAKE_CONFIGURE_FLAGS=-G "%CMAKE_GENERATOR%%CMAKE_GENERATOR_SUFFIX%"
+set CMAKE_BUILD_FLAGS="%CONFIGURATION%"
