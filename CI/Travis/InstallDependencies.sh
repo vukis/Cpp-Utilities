01344e7a86216e90524a59d61f88a3195ad5b8d0
vukis <vukis@bk.ru>
2017-07-26 02:09:14 +0300

    fix build

--- CI/Travis/InstallDependencies.sh
+++ CI/Travis/InstallDependencies.sh
@@ -7,6 +7,3 @@ sudo ./bootstrap.sh --with-libraries=filesystem,system
 sudo ./b2 toolset=gcc link=shared threading=multi variant=$BUILD_CONFIGURATION
 sudo ./b2 install
 cd ..
-
-# install codecov
-sudo update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-5 90
