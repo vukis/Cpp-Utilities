cf74b4d76cae3e1e7c0e9258b25e857817ff5195
vukis <vukis@bk.ru>
2017-07-28 01:17:04 +0300

    [CI] fix

--- CI/Travis/InstallDependencies.sh
+++ CI/Travis/InstallDependencies.sh
@@ -7,3 +7,5 @@ sudo ./bootstrap.sh --with-libraries=filesystem,system
 sudo ./b2 toolset=gcc link=shared threading=multi variant=$BUILD_CONFIGURATION
 sudo ./b2 install
 cd ..
+
+# sudo apt-get install libboost1.55-all-dev
