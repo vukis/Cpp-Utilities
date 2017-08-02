2fcf2183597915f1d1f0676bcb6bc210f16db53a
vukis <vukis@bk.ru>
2017-08-02 03:08:44 +0300

    fix

--- CMakeModules/CppCheck.cmake
+++ CMakeModules/CppCheck.cmake
@@ -4,7 +4,6 @@ add_custom_target(
         cppcheck
         COMMAND /usr/bin/cppcheck
 		--enable=all
- #       --enable=warning,performance,portability,information,missingInclude
   #      --std=c++11
    #     --library=qt.cfg
         --template="[{severity}][{id}] {message} {callstack} \(On {file}:{line}\)"
