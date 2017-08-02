b9ed262dcd54ada03b04849d2ccfa7936e664184
vukis <vukis@bk.ru>
2017-08-02 21:52:08 +0300

    Revert "increase number of tryouts to steal the work"
    This reverts commit 4ea9afce65663b948776bef05e3f1d651319572e.

--- ThreadPool/WorkStealingThreadPool.h
+++ ThreadPool/WorkStealingThreadPool.h
@@ -35,7 +35,7 @@ private:
 
     std::vector<TaskQueue> m_queues;
     std::atomic<size_t>    m_queueIndex{ 0 };
-    const size_t m_tryoutCount{ 5 };
+    const size_t m_tryoutCount{ 1 };
 
     std::vector<std::thread> m_threads;
 };
