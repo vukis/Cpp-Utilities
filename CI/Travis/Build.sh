5c40f866a0bc169d692b4f7c6d0687c5b2a0a889
vukis <vukis@bk.ru>
2017-08-02 14:29:54 +0300

    comment fix

--- CI/Travis/Build.sh
+++ CI/Travis/Build.sh
@@ -21,8 +21,8 @@ if [ $COVERAGE == "On" ]; then
   make gcov && make lcov
   # Creating report
   cd $TRAVIS_BUILD_DIR 
-  lcov --directory . --capture --output-file coverage.info # Capture coverage info;
-  lcov --remove coverage.info '/usr/*' --output-file coverage.info # Filter out system;
+  lcov --directory . --capture --output-file coverage.info # Capture coverage info
+  lcov --remove coverage.info '/usr/*' --output-file coverage.info # Filter out system
   lcov --list coverage.info #Debug info
   echo Uploading report to CodeCov.io...
   bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
