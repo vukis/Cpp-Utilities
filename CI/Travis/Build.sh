b77c914518e23bb650718cf4713abd6ef771fb06
vukis <vukis@bk.ru>
2017-08-02 12:45:09 +0300

    Fix in build script

--- CI/Travis/Build.sh
+++ CI/Travis/Build.sh
@@ -9,6 +9,12 @@ make
 echo Run tests...
 ctest -C $BUILD_CONFIGURATION --output-on-failure
 
+# Static analysis
+if [ $BUILD_CONFIGURATION == "debug" ]; then
+  echo Run cppcheck...
+  make cppcheck
+fi
+
 # Code covarage
 if [ $COVERAGE == "On" ]; then
   echo Run code coverage...
@@ -18,15 +24,6 @@ if [ $COVERAGE == "On" ]; then
   lcov --directory . --capture --output-file coverage.info # Capture coverage info;
   lcov --remove coverage.info '/usr/*' --output-file coverage.info # Filter out system;
   lcov --list coverage.info #Debug info
-fi
-
-# Static analysis
-if [ $BUILD_CONFIGURATION == "debug" ]; then
-  echo Run cppcheck...
-  make cppcheck
-fi
-
-if [ $COVERAGE == "On" ]; then
   echo Uploading report to CodeCov.io...
   bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
 fi
