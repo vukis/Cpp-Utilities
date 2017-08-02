e1cb93e2762611daff7475a202d0950b6b7d172c
vukis <vukis@bk.ru>
2017-08-02 13:02:36 +0300

    Update cppcheck parameters

--- CMakeModules/CppCheck.cmake
+++ CMakeModules/CppCheck.cmake
@@ -4,12 +4,14 @@ add_custom_target(
         cppcheck
         COMMAND /usr/bin/cppcheck
 		--enable=all
-  #      --std=c++11
-   #     --library=qt.cfg
+		--suppress=missingIncludeSystem
+        --std=c++11
         --template="[{severity}][{id}] {message} {callstack} \(On {file}:{line}\)"
         --verbose
-   #     --quiet
-  #      --error-exitcode=1
 		--check-config
+	    # --error-exitcode=1
+		# -- I [dir] // to include STL/Boost/Windows headers
+        # --quiet
+        # --library=qt.cfg
         ${ALL_SOURCE_FILES}
 )
