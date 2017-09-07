7c1776eeb5ed729c916dc772b7d174ae2352c80c
vukis <vukis@bk.ru>
2017-09-07 16:13:54 +0300

    install boost with system and filesystem

--- CI/Travis/InstallDependencies.sh
+++ CI/Travis/InstallDependencies.sh
@@ -5,7 +5,7 @@ echo Install Boost 1.64.0...
 sudo wget -O boost_1_64_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.64.0/boost_1_64_0.tar.gz/download
 sudo tar xzf boost_1_64_0.tar.gz
 cd boost_1_64_0/
-sudo ./bootstrap.sh # --with-libraries=filesystem,system
+sudo ./bootstrap.sh --with-libraries=system,filesystem
 sudo ./b2 toolset=gcc link=shared threading=multi variant=$BUILD_CONFIGURATION
 sudo ./b2 install
 cd ..
